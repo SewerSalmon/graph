9921b2ba8dcf524556f8cf39b29bf521eb9f74f4
SewerSalmon <bj.jwalker2004@gmail.com>
2022-05-24 10:35:18 +0100

    Initial commit

---
+++ src/com/company/Basic.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+public class Basic extends JPanel {
+    private JFrame frame;
+    private int[] data;
+    public Basic(int height,int width, int[] Data){
+        frame = new JFrame("Basic Graph");
+        frame.add(this);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(width,height);
+        frame.setVisible(true);
+        data = Data;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        int left = 50;    // hard-coded just for testing
+        int top = 80;
+        int width = data.length*10;
+        int height = 100;
+        g.setColor(Color.BLACK);
+        g.fillRect(left, top, width, height);
+        g.setColor(Color.BLUE);
+        for (int x = 0;x<data.length;x++ )
+        g.fillRect(left+(x)*10,top+height-data[x],5,data[x]);
+
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+
+package com.company;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("start");
+        GUImain gui = new GUImain();
+        System.out.println("end");
+    }
+}
+
+
+
+
+
